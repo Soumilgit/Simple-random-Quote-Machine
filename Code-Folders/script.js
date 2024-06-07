b29f17d2bc35223bbc622cbcc11b22115cc2100c
Soumil Mukhopadhyay <soumil.m@somaiya.edu>
2024-06-07 16:37:38 +0530

    Update script.js

--- Code-Folders/script.js
+++ Code-Folders/script.js
@@ -25,9 +25,12 @@ var currentQuote = '',
   currentAuthor = '';
 
 function fetchRandomQuote() {
+  $('#loading-spinner').show();
   fetch('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json')
   .then(response => response.json())
   .then(data => {
+
+      $('#loading-spinner').hide();
       const quote = data.quotes[Math.floor(Math.random() * data.quotes.length)];
       currentQuote = quote.quote;
       currentAuthor = quote.author;
@@ -35,6 +38,7 @@ function fetchRandomQuote() {
     })
   .catch(error => {
       console.error('Error fetching quote:', error);
+      $('#loading-spinner').show();
     });
 }
 
@@ -55,10 +59,15 @@ function updateUI(quoteObj) {
       encodeURIComponent(currentQuote) +
       '&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button'
   );
-
   
-  $('.quote-text').text(currentQuote);
+  
+   $('.quote-text').text(currentQuote);
   $('.quote-author').text('- ' + currentAuthor);
+  $('.quote-text,.quote-author').fadeOut(function() {
+    /* $(this).text(quoteObj.quote + ' - ' + quoteObj.author); */
+    // Fade in the new quote
+    $(this).fadeIn();
+  });
 
   
   var color = colors[Math.floor(Math.random() * colors.length)];
@@ -66,6 +75,17 @@ function updateUI(quoteObj) {
   document.body.style.color = color; 
   $('.button').css('background-color', color); 
 }
+function changeTheme(color) {
+  document.body.style.backgroundColor = color;
+  document.body.style.color = color;
+  $('.button').css('background-color', color);
+}
+
+document.getElementById("new-quote").addEventListener("click", fetchRandomQuote);
+
+
+changeTheme('#8A2BE2'); 
+
   
 
 
